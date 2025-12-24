a943439c888c3953a9c7e0724f95b33ee78dbbfb
mserenadigennaro <maseredigenn@hotmail.com>
2025-12-24 10:09:18 +0100

    boolean

---
+++ booleano1.py
@@ -0,0 +1,4 @@
+age=int(input("Quanti anni hai?  "))
+licence=input("Hai la patente? Rispondi con si o con no  ")
+print("Puoi guidare: ", age>=18 and licence=="si")
+ 
---
+++ booleano2.py
@@ -0,0 +1,6 @@
+bookback=input("Hai restituito l'ultimo prestito " \
+"entro la scadenza? Rispondi con si o con no  ")
+subscription=input("Hai l'abbonamento premium? " \
+"Rispondi con si o con no  ")
+print("Puoi entrare in biblioteca: ", bookback=="si" 
+    or subscription=="si")
